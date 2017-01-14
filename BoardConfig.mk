3f626def6d5083d44df3fe7bf565fdcb25806c16
Nuno Silva <nafsilva@gmail.com>
2017-01-14 03:49:14 +0000

    Fix sdclang

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ ifneq ($(HOST_OS),darwin)
 
 SDCLANG := true
 
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin
+SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8/bin/bin
 
 SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
 
